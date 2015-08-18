96a6f6d76545d1b286ffb88cebdd48d6e20eb707
Robey Pointer <robeypointer@gmail.com>
2015-08-18 14:39:52 -0700

    convert to ES6 and add background color.

---
+++ src/clicolor.js
@@ -0,0 +1,96 @@
+"use strict";
+
+import antsy from "antsy";
+
+export const STYLES = {
+  dim: "888",
+  timestamp: "0cc",
+  error: "c00",
+  warning: "f60"
+};
+
+class CliColor {
+  constructor() {
+    this._useColor = process.stdout.isTTY;
+    this._quiet = false;
+  }
+
+  useColor(x) {
+    this._useColor = x;
+  }
+
+  quiet(x) {
+    this._quiet = x;
+  }
+
+  display(...message) {
+    const text = (message.length == 1 ? message[0] : this.paint(...message)).toString();
+    process.stdout.write(text + "\n");
+  }
+
+  displayNotice(...message) {
+    const text = (message.length == 1 ? message[0] : this.paint(...message)).toString();
+    if (!this._quiet) process.stderr.write(text + "\n");
+  }
+
+  displayError(...message) {
+    this.display(this.color(STYLES.error, "ERROR"), ": ", ...message);
+  }
+
+  displayWarning(message) {
+    this.display(this.color(STYLES.warning, "WARNING"), ": ", ...message);
+  }
+
+  paint(...spans) {
+    return new Span(null, spans, this._useColor);
+  }
+
+  color(colorName, ...spans) {
+    return new Span(colorName, spans, this._useColor);
+  }
+
+  backgroundColor(colorName, ...spans) {
+    return new Span("bg:" + colorName, spans, this._useColor);
+  }
+
+  bgColor(colorName, ...spans) {
+    return this.backgroundColor(colorName, ...spans);
+  }
+}
+
+class Span {
+  constructor(color, spans, _useColor) {
+    this.color = color;
+    this.spans = spans;
+    this._useColor = _useColor;
+  }
+
+  toString() {
+    let escOn = "", escOff = "";
+    if (this.color && this._useColor) {
+      switch (this.color) {
+        case "underline":
+          escOn = "\u001b[4m";
+          escOff = "\u001b[24m";
+          break;
+        default:
+          let colorName = this.color;
+          const match = this.color.match(/^bg:(.*)$/);
+          if (match) {
+            colorName = match[1];
+            const c = antsy.get_color(STYLES[colorName] ? STYLES[colorName] : colorName);
+            escOn = `\u001b[48;5;${c}m`;
+            escOff = `\u001b[49m`;
+          } else {
+            const c = antsy.get_color(STYLES[colorName] ? STYLES[colorName] : colorName);
+            escOn = `\u001b[38;5;${c}m`;
+            escOff = `\u001b[39m`;
+          }
+          break;
+      }
+    }
+    return escOn + this.spans.map(span => span.toString()).join(escOn) + escOff;
+  }
+}
+
+export const clicolor = () => new CliColor();
